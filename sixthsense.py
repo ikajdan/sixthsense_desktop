805894f312ffb362edaa43e2e68287b4ae2a5cdd
Ignacy Kajdan <ignacy.kajdan@gmail.com>
2023-05-29 15:52:45 +0200

    Add settings view

--- sixthsense.py
+++ sixthsense.py
@@ -10,6 +10,57 @@ from matplotlib.backends.backend_gtk3agg import FigureCanvasGTK3Agg as FigureCan
 
 import matplotlib.style as mpl_style
 
+host_name_pref = "localhost"
+port_number_pref = 8000
+refresh_time_pref = 1000
+
+
+class SettingsView(Gtk.Box):
+    def __init__(self):
+        Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL, spacing=20)
+
+        self.grid = Gtk.Grid()
+        self.grid.set_column_homogeneous(True)
+        self.grid.set_row_spacing(6)
+        self.pack_start(self.grid, False, False, 0)
+
+        self.host_name_label = Gtk.Label(label="Hostname:")
+        self.port_number_label = Gtk.Label(label="Port:")
+        self.refresh_time_label = Gtk.Label(label="Sampling Time:")
+
+        self.host_entry = Gtk.Entry()
+        self.port_entry = Gtk.Entry()
+        self.refresh_time_entry = Gtk.Entry()
+
+        save_button = Gtk.Button(label="Save")
+        save_button.connect("clicked", self.save_settings)
+
+        self.grid.attach(self.create_aligned_label(self.host_name_label), 0, 0, 2, 1)
+        self.grid.attach(self.host_entry, 0, 1, 2, 1)
+        self.grid.attach(self.create_aligned_label(self.port_number_label), 0, 2, 2, 1)
+        self.grid.attach(self.port_entry, 0, 3, 2, 1)
+        self.grid.attach(self.create_aligned_label(self.refresh_time_label), 0, 4, 2, 1)
+        self.grid.attach(self.refresh_time_entry, 0, 5, 2, 1)
+        self.pack_end(save_button, False, False, 0)
+
+        self.restore_settings()
+
+    def create_aligned_label(self, label):
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        hbox.pack_start(label, False, False, 0)
+        return hbox
+
+    def save_settings(self, widget):
+        global host_name_pref, port_number_pref, refresh_time_pref
+        host_name_pref = self.host_entry.get_text()
+        port_number_pref = self.port_entry.get_text()
+        refresh_time_pref = self.refresh_time_entry.get_text()
+
+    def restore_settings(self):
+        self.host_entry.set_text(host_name_pref)
+        self.port_entry.set_text(str(port_number_pref))
+        self.refresh_time_entry.set_text(str(refresh_time_pref))
+
 
 class SixthSense(Gtk.Window):
     def __init__(self):
@@ -24,17 +75,23 @@ class SixthSense(Gtk.Window):
         self.view1 = Gtk.Label(label="Sensors")
         self.view2 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.view3 = Gtk.Label(label="Control")
-        self.view4 = Gtk.Label(label="Settings")
+        self.settings_view = Gtk.Label(label="Settings")
+        self.settings_view = SettingsView()
 
         self.stack.add_named(self.view1, "Sensors")
         self.stack.add_named(self.view2, "Plots")
         self.stack.add_named(self.view3, "Control")
-        self.stack.add_named(self.view4, "Settings")
+        self.stack.add_named(self.settings_view, "Settings")
 
+        # TODO: Move this to a separate function
         self.view2.set_margin_top(20)
         self.view2.set_margin_end(10)
         self.view2.set_margin_bottom(20)
         self.view2.set_margin_start(10)
+        self.settings_view.set_margin_top(20)
+        self.settings_view.set_margin_end(10)
+        self.settings_view.set_margin_bottom(20)
+        self.settings_view.set_margin_start(10)
 
         self.button1 = Gtk.Button(label="Sensors")
         self.button1.connect("clicked", self.switch_view, "Sensors")
@@ -85,10 +142,10 @@ class SixthSense(Gtk.Window):
         # Horizontal box for buttons
         buttons_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=20)
         self.box.pack_start(buttons_box, False, False, 0)
-        buttons_box.set_margin_bottom(5)
         buttons_box.set_margin_top(10)
-        buttons_box.set_margin_left(10)
-        buttons_box.set_margin_right(10)
+        buttons_box.set_margin_end(10)
+        buttons_box.set_margin_bottom(5)
+        buttons_box.set_margin_start(10)
 
         buttons_box.pack_start(self.button1, True, True, 0)
         buttons_box.pack_start(self.button2, True, True, 0)
@@ -102,7 +159,11 @@ class SixthSense(Gtk.Window):
 
     def updateSensorsPlot(self):
         # Fetch data from the REST API
-        response = requests.get("http://localhost:8000/sensors/all?t=c&p=hpa&h=perc")
+
+        api_endpoint = (
+            "http://" + host_name_pref + ":" + str(port_number_pref) + "/sensors/all"
+        )
+        response = requests.get(api_endpoint)
 
         # Parse the data and add it to the plot
         self.temperature = response.json()["temperature"]["value"]
@@ -120,17 +181,14 @@ class SixthSense(Gtk.Window):
         self.pressure_data = self.pressure_data[-10:]
         self.humidity_data = self.humidity_data[-10:]
 
-        # Update the plots
         self.line1.set_data(self.x_data, self.temperature_data)
 
-        # Autoscale the plots
         self.axis.relim()
         self.axis.autoscale_view(True, True, True)
 
         self.canvas.draw()
 
-        # Schedule the next data fetch
-        GLib.timeout_add_seconds(1, self.updateSensorsPlot)
+        GLib.timeout_add(refresh_time_pref, self.updateSensorsPlot)
 
 
 window = SixthSense()
